10fb7d2938fd0f93aff904656bc82fdb9e3696fe
Lasse Larsen <iamlasse@gmail.com>
2024-09-10 15:41:08 +0200

    Install hybridly

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,12 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->appendToGroup('web', [\App\Http\Middleware\HandleHybridRequests::class]);
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
