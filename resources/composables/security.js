082006ede25efae732d737e8a8d16a8a13ea712a
Lasse Larsen <iamlasse@gmail.com>
2024-09-24 16:38:26 +0200

    Remove built files

--- resources/composables/security.js
+++
@@ -1,9 +0,0 @@
-export default function useSecurity() {
-    const user = useProperty('security.user');
-    const authenticated = computed(() => !!user.value);
-    return {
-        user,
-        authenticated,
-    };
-}
-//# sourceMappingURL=security.js.map
