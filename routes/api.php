4ec15408ac65ee03f39844d3c24ac2af8ef13c9e
Lasse Larsen <iamlasse@gmail.com>
2024-10-08 13:25:25 +0200

    Something else

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'loginApi'])->middleware('guest')->name('login');
